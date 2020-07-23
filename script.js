322a0550a73d4ba28210e432f9a7fef3cd71f8f9
jiblotech <52890149+jiblotech@users.noreply.github.com>
2020-07-23 04:21:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+	function changeRed(){
+		document.getElementById('body').style.backgroundColor='red';
+	}
+	function changeBlue(){
+		document.getElementById('body').style.backgroundColor='Blue';
+	}
+	function changeGreen(){
+		document.getElementById('body').style.backgroundColor='Green';
+	}
+	function changeViolet(){
+		document.getElementById('body').style.backgroundColor='Violet';
+	}
+	function changePurple(){
+		document.getElementById('body').style.backgroundColor='Purple';
+	}
+	function changeBack(){
+		document.getElementById('body').style.backgroundColor='white';
+	}
+	
